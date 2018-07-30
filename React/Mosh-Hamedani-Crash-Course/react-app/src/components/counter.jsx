98c85d76c25c83c3176e641d9e73fee8cbccd375
Ioannis Leontiadis <giannisleondiadis@gmail.com>
2018-07-30 14:55:52 +0300

    Increment count of Counter on click

--- React/Mosh-Hamedani-Crash-Course/react-app/src/components/counter.jsx
+++ React/Mosh-Hamedani-Crash-Course/react-app/src/components/counter.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  }
+
+  handleClick() {
+    this.setState({count: this.state.count + 1})
+  }
+
   render () {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <span>{this.state.count}</span>
+        <button onClick={() => this.handleClick()}>Increment</button>
       </React.Fragment>
     )
   }
