ee947e859fab2e6ddc21c3eb955fc19c258507ce
Ioannis Leontiadis <giannisleondiadis@gmail.com>
2018-07-30 15:57:30 +0300

    Specify bootstrap classes for the components of Counter

--- React/Mosh-Hamedani-Crash-Course/react-app/src/components/counter.jsx
+++ React/Mosh-Hamedani-Crash-Course/react-app/src/components/counter.jsx
@@ -2,18 +2,29 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 0
   }
 
-  handleClick() {
+  handleClick () {
     this.setState({count: this.state.count + 1})
   }
 
   render () {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
-        <button onClick={() => this.handleClick()}>Increment</button>
+        <span
+          className='badge badge-primary m-2'
+        >
+          {this.state.count}
+        </span>
+
+        <button
+          onClick={() => this.handleClick()}
+          className='btn btn-secondary btn-sm'
+        >
+          Increment
+        </button>
+
       </React.Fragment>
     )
   }
