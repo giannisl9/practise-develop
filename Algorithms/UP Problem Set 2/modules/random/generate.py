04ca4c73ce89e60fb49b462664327fc7d4419ffd
Ioannis Leontiadis <giannisleondiadis@gmail.com>
2019-05-04 23:53:29 +0300

    Modify random edge generator to be parametrizable

--- Algorithms/UP Problem Set 2/modules/random/generate.py
+++ Algorithms/UP Problem Set 2/modules/random/generate.py
@@ -2,12 +2,12 @@ import itertools
 import random
 
 random.seed(1046836)
-tmp = list(itertools.permutations(range(0,999), 2))
-sample_space = []
 
-def generate_edges():
+def generate_edges(number_of_vertices, number_of_edges, val_min, val_max):
+    tmp = list(itertools.permutations(range(0,number_of_vertices), 2))
+    sample_space = []
     for elem in tmp:
-        for x in range(10, 50):
+        for x in range(val_min, val_max + 1):
             sample_space.append((elem[0], elem[1], x))
-    return random.sample(sample_space, 10000)
+    return random.sample(sample_space, number_of_edges)
 
