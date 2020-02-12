34337c765d46926afdba7e8cc30a01d114588bc4
Ioannis Leontiadis <giannisleondiadis@gmail.com>
2020-02-12 06:04:40 +0200

    Delete duplicate React folder

--- React/snippet-code/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Argument from './components/argumentToEventHandler.jsx'
-
-ReactDOM.render(<Argument />, document.getElementById('root'))
